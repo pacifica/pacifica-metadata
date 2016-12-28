0048fa5fd22923daf16739a90ae340db548bec05
David Brown <dmlb2000@gmail.com>
2016-12-28 10:38:18 -0800

    fix #106 add default error page (#113)
    This should make sure all error that show up return as json objects

--- MetadataServer.py
+++ MetadataServer.py
@@ -1,8 +1,28 @@
 #!/usr/bin/python
 """Main metadata server script."""
+from json import dumps
 import cherrypy
 from metadata.rest.root import Root
 from metadata.orm import create_tables
 
-create_tables()
-cherrypy.quickstart(Root(), '/', 'server.conf')
+
+def error_page_default(**kwargs):
+    """The default error page should always enforce json."""
+    cherrypy.response.headers['Content-Type'] = 'application/json'
+    return dumps({
+        'status': kwargs['status'],
+        'message': kwargs['message'],
+        'traceback': kwargs['traceback'],
+        'version': kwargs['version']
+    })
+
+
+def main():
+    """Main method to start the httpd server."""
+    create_tables()
+    cherrypy.config.update({'error_page.default': error_page_default})
+    cherrypy.quickstart(Root(), '/', 'server.conf')
+
+
+if __name__ == '__main__':
+    main()
