e61e4991c366cfb1594864019827796ff18affa5
Ken Auberry <kenneth.auberry@pnnl.gov>
2017-02-21 14:13:35 -0800

    Adding support for code to get the last available transaction id

---
+++ metadata/rest/transaction_queries/transaction_last.py
@@ -0,0 +1,33 @@
+"""CherryPy Status Transaction Metadata object class."""
+from cherrypy import tools
+from peewee import fn
+from metadata.rest.transaction_queries.query_base import QueryBase
+from metadata.orm import Transactions
+from metadata.orm.base import db_connection_decorator
+
+# pylint: disable=too-few-public-methods
+
+
+class TransactionLast(QueryBase):
+    """Retrieves details for an individual transaction."""
+
+    exposed = True
+
+    @staticmethod
+    def _get_last_known_transaction():
+        txn_id = (Transactions
+                  .select(fn.Max(Transactions.id))
+                  .where(Transactions.deleted >> None)
+                  .dicts()
+                  .get())
+        print 'transaction_id = {0}'.format(txn_id)
+        return {'latest_transaction_id': txn_id['id']}
+
+    # Cherrypy requires these named methods.
+    # pylint: disable=invalid-name
+    @staticmethod
+    @tools.json_out()
+    @db_connection_decorator
+    def GET():
+        """Return details about the specified transaction entity."""
+        return TransactionLast._get_last_known_transaction()
