b5329ecaf8f6d30ab58f85eaf6b49d67a4fb7b15
David Brown <dmlb2000@gmail.com>
2017-03-05 20:00:58 -0800

    make this optional for collaboration

--- travis/unit-tests.sh
+++ travis/unit-tests.sh
@@ -2,4 +2,6 @@
 coverage run --include='metadata/*' -m pytest -v metadata/orm metadata/elastic metadata/test_client.py
 coverage run --include='metadata/*' -a -m pytest -v metadata/rest
 coverage report --show-missing --fail-under 100
-codeclimate-test-reporter
+if [[ $CODECLIMATE_REPO_TOKEN ]] ; then
+  codeclimate-test-reporter
+fi
