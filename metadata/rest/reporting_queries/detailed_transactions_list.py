350d06f5412914a03a0bc60b8ad3fb7cd9bf7b41
Ken Auberry <kenneth.auberry@pnnl.gov>
2017-04-10 09:52:59 -0700

    Adding detailed_transaction_list support

---
+++ metadata/rest/reporting_queries/detailed_transactions_list.py
@@ -0,0 +1,54 @@
+"""CherryPy Status Metadata object class."""
+from cherrypy import tools, request
+from peewee import fn
+from metadata.rest.reporting_queries.query_base import QueryBase
+from metadata.orm import Transactions, Files
+
+
+# pylint: disable=too-few-public-methods
+class DetailedTransactionList(QueryBase):
+    """Retrieves a list of all transactions matching the search criteria."""
+
+    exposed = True
+
+    @staticmethod
+    def _get_transaction_list_details(transaction_list):
+        query = (Files().select(
+            Files.transaction.alias('upload_id'),
+            fn.Max(Transactions.updated).alias('upload_date'),
+            fn.Min(Files.mtime).alias('file_date_start'),
+            fn.Max(Files.mtime).alias('file_date_end'),
+            fn.Min(Transactions.submitter).alias('uploaded_by_id'),
+            fn.Sum(Files.size).alias('bundle_size'),
+            fn.Count(Files.id).alias('file_count'),
+            fn.Min(Transactions.updated).alias('upload_datetime'),
+            fn.Min(Transactions.proposal).alias('proposal_id'),
+            fn.Min(Transactions.instrument).alias('instrument_id'))
+                 .join(Transactions)
+                 .where(Files.transaction << transaction_list)
+                 .group_by(Files.transaction))
+
+        return {str(r['upload_id']): {
+            'upload_id': str(r['upload_id']),
+            'upload_date': r['upload_date'].date().strftime('%Y-%m-%d'),
+            'file_date_start': r['file_date_start'].date().strftime('%Y-%m-%d'),
+            'file_date_end': r['file_date_end'].date().strftime('%Y-%m-%d'),
+            'uploaded_by_id': int(r['uploaded_by_id']),
+            'bundle_size': int(r['bundle_size']),
+            'file_count': int(r['file_count']),
+            'upload_datetime': r['upload_date'].strftime('%Y-%m-%d %H:%M:%S'),
+            'proposal_id': r['proposal_id'],
+            'instrument_id': r['instrument_id']
+        } for r in query.dicts()}
+
+    # Cherrypy requires these named methods.
+    # pylint: disable=invalid-name
+    @staticmethod
+    @tools.json_in()
+    @tools.json_out()
+    def POST():
+        """Return summaryinfo for a given object type/id/time range combo."""
+        # parse object list
+        transaction_list = request.json
+
+        return DetailedTransactionList._get_transaction_list_details(transaction_list)
---
+++ metadata/rest/reporting_queries/query_base.py
@@ -0,0 +1,21 @@
+"""CherryPy Status Metadata proposalinfo base class."""
+import pytz
+
+
+# pylint: disable=too-few-public-methods
+class QueryBase(object):
+    """Formats summary data for other classes down the tree."""
+
+    time_basis_mappings = {
+        'modified': 'mtime',
+        'created': 'ctime',
+        'submitted': 'updated'
+    }
+
+    object_type_mappings = {
+        'instrument': 'instrument',
+        'proposal': 'proposal',
+        'user': 'submitter'
+    }
+
+    local_timezone = pytz.timezone('America/Los_Angeles')
