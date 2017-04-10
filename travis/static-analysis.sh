31875ec8b5656cce99dc79f98ec8a23912827572
Ken Auberry <kenneth.auberry@pnnl.gov>
2017-04-10 09:53:41 -0700

    Adding options to user lookups

--- travis/static-analysis.sh
+++ travis/static-analysis.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-pylint --rcfile=pylintrc metadata
-pylint --rcfile=pylintrc MetadataServer.py GenMetadataModelMD.py
+pylint --rcfile=pylintrc --disable=I metadata
+pylint --rcfile=pylintrc --disable=I MetadataServer.py GenMetadataModelMD.py
 radon cc metadata
