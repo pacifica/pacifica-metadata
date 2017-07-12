24e760842c246d2286ac643d8616dd48ced2c467
dmlb2000 <dmlb2000@gmail.com>
2017-07-12 13:48:21 -0700

    should not contain EUS anywhere

--- metadata/rest/user_queries/query_base.py
+++ metadata/rest/user_queries/query_base.py
@@ -24,7 +24,7 @@ class QueryBase(object):
             prop_id = prop.id
             clean_proposals[prop_id] = info
 
-        display_name = '[EUS ID {0}] {1} {2} &lt;{3}&gt;'.format(
+        display_name = '[User ID {0}] {1} {2} &lt;{3}&gt;'.format(
             user_entry.id,
             user_hash.get('first_name'),
             user_hash.get('last_name'),
