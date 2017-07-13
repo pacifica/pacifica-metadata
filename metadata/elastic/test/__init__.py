9977c86274dfce9e06d2d1ca123538681704e96f
dmlb2000 <dmlb2000@gmail.com>
2017-07-13 14:56:47 -0700

    add the cluster checks for having the client talk to nodes

--- metadata/elastic/test/__init__.py
+++ metadata/elastic/test/__init__.py
@@ -1,2 +1,22 @@
 #!/usr/bin/python
 """Tests for the Elastic Search module."""
+
+ES_CLUSTER_BODY = {
+    'cluster_name': 'elasticsearch',
+    'nodes': {
+        'qQooKRbMTbqhyaTx_qWQmw': {
+            'name': 'Ivan Kragoff',
+            'transport_address': '127.0.0.1:9300',
+            'host': '127.0.0.1',
+            'ip': '127.0.0.1',
+            'version': '2.4.5',
+            'build': 'c849dd1',
+            'http_address': '127.0.0.1:9200',
+            'http': {
+                'bound_address': ['0.0.0.0:9200'],
+                'publish_address': '127.0.0.1:9200',
+                'max_content_length_in_bytes': 104857600
+            }
+        }
+    }
+}
