0b1f66afd453fb7e5af028253315affbaef2e0f2
David Brown <dmlb2000@gmail.com>
2017-11-23 15:44:21 -0800

    check for parts already not there

--- metadata/rest/orm.py
+++ metadata/rest/orm.py
@@ -149,7 +149,8 @@ class CherryPyAPI(PacificaModel, ElasticAPI):
             if '_id' in obj.keys() and obj['_id'] is not None:
                 new_obj['id'] = obj.get('_id')
             for attr in model_info.get('related_names'):
-                del new_obj[attr]
+                if attr in new_obj:
+                    del new_obj[attr]
             new_obj.pop('_id')
             clean_objs['upload_objs'].append(new_obj)
 
