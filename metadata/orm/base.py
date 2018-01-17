49f06604c9f73bd95589bacd0f8837118edb917f
Ken Auberry <kenneth.auberry@pnnl.gov>
2018-01-17 14:59:29 -0800

    tweaks

--- metadata/orm/base.py
+++ metadata/orm/base.py
@@ -237,10 +237,9 @@ class PacificaModel(Model):
     def last_change_date(cls):
         """Find the last changed date for the object."""
         last_change_date = cls.select(fn.Max(cls.updated)).scalar()
-        if last_change_date is None:
-            return '1970-01-01 00:00:00'
-        last_change_string = last_change_date.isoformat(' ') if isinstance(last_change_date, datetime.datetime)
-        else last_change_date
+        last_change_string = '1970-01-01 00:00:00'
+        last_change_string = last_change_date.isoformat(' ') \
+            if isinstance(last_change_date, datetime.datetime) else last_change_string
 
         return last_change_string
 
