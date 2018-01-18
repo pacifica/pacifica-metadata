aeb7a1004fbb19ff0545e7052630fe93d3e977ab
Ken Auberry <kenneth.auberry@pnnl.gov>
2018-01-18 15:26:17 -0800

    isoformat fix

--- metadata/orm/base.py
+++ metadata/orm/base.py
@@ -240,7 +240,7 @@ class PacificaModel(Model):
         last_change_string = last_change_date \
             if last_change_date is not None else '1970-01-01 00:00:00'
         last_change_string = last_change_date.isoformat(' ') \
-            if isinstance(last_change_date, datetime.datetime) else parser.parse(last_change_string).isoformat()
+            if isinstance(last_change_date, datetime.datetime) else parser.parse(last_change_string).isoformat(' ')
         return unicode(last_change_string)
 
     @classmethod
