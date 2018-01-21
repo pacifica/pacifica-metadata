15db09636e3306894d0abcb9a20fca3faa0565a4
David Brown <dmlb2000@gmail.com>
2018-01-21 12:09:25 -0800

    add admin tools to run internal methods

---
+++ metadata/admin_cmd.py
@@ -0,0 +1,58 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+"""Admin Module for Admin Commands."""
+from __future__ import print_function
+from argparse import ArgumentParser
+from metadata.orm import ORM_OBJECTS, try_db_connect
+from metadata.elastic import create_elastic_index, try_es_connect
+
+
+def escreate(args):
+    """Create the elastic search index and mappings."""
+    try_db_connect()
+    try_es_connect()
+    create_elastic_index()
+    if args.skip_mappings:
+        return
+    for obj in ORM_OBJECTS:
+        obj.create_elastic_mapping()
+
+
+def essync(args):
+    """Sync the elastic search data from sql to es."""
+    print(args.threads)
+
+
+def main():
+    """Main method for admin command line tool."""
+    parser = ArgumentParser()
+    subparsers = parser.add_subparsers(help='sub-command help')
+    escreate_parser = subparsers.add_parser(
+        'escreate',
+        help='escreate help',
+        description='create elastic index and mappings'
+    )
+    essync_parser = subparsers.add_parser(
+        'essync',
+        help='essync help',
+        description='sync sql data to elastic search'
+    )
+    essync_parser.add_argument(
+        '--threads',
+        default=4,
+        type=int,
+        help='number of threads to sync data',
+        required=False
+    )
+    escreate_parser.add_argument(
+        '--skip-mappings',
+        help='skip creating mappings',
+        default=False,
+        action='store_true',
+        dest='skip_mappings',
+        required=False
+    )
+    escreate_parser.set_defaults(func=escreate)
+    essync_parser.set_defaults(func=essync)
+    args = parser.parse_args()
+    args.func(args)
--- setup.py
+++ setup.py
@@ -2,37 +2,25 @@
 # -*- coding: utf-8 -*-
 """Setup and install the metadata."""
 from pip.req import parse_requirements
-from setuptools import setup
+from setuptools import setup, find_packages
 
 # parse_requirements() returns generator of pip.req.InstallRequirement objects
 INSTALL_REQS = parse_requirements('requirements.txt', session='hack')
 
-setup(name='PacificaMetadata',
-      use_scm_version=True,
-      setup_requires=['setuptools_scm'],
-      description='Pacifica Metadata',
-      author='David Brown',
-      author_email='david.brown@pnnl.gov',
-      packages=[
-          'metadata',
-          'metadata.elastic',
-          'metadata.elastic.test',
-          'metadata.orm',
-          'metadata.orm.test',
-          'metadata.rest',
-          'metadata.rest.fileinfo_queries',
-          'metadata.rest.instrument_queries',
-          'metadata.rest.migration_queries',
-          'metadata.rest.proposal_queries',
-          'metadata.rest.reporting_queries',
-          'metadata.rest.test',
-          'metadata.rest.tkvinfo_queries',
-          'metadata.rest.tkvupload_queries',
-          'metadata.rest.transaction_queries',
-          'metadata.rest.user_queries'
-      ],
-      scripts=['MetadataServer.py', 'DatabaseCreate.py'],
-      entry_point={
-          'console_scripts': ['MetadataServer=metadata:main']
-      },
-      install_requires=[str(ir.req) for ir in INSTALL_REQS])
+setup(
+    name='PacificaMetadata',
+    use_scm_version=True,
+    setup_requires=['setuptools_scm'],
+    description='Pacifica Metadata',
+    author='David Brown',
+    author_email='david.brown@pnnl.gov',
+    packages=find_packages(),
+    scripts=['MetadataServer.py', 'DatabaseCreate.py'],
+    entry_points={
+        'console_scripts': [
+            'MetadataServer=metadata:main'
+            'MDAdmin=metadata.admin_cmd:main'
+        ]
+    },
+    install_requires=[str(ir.req) for ir in INSTALL_REQS]
+)
