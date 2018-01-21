537eb4c9e2cc15ea1ae566f39937ed469de1c5e6
David Brown <dmlb2000@gmail.com>
2018-01-21 14:40:08 -0800

    mock the thing might be less code lines

--- metadata/test/test_admin.py
+++ metadata/test/test_admin.py
@@ -5,9 +5,6 @@ import os
 from argparse import Namespace
 from unittest import TestCase
 from mock import patch
-from peewee import SqliteDatabase
-from playhouse.test_utils import test_database
-import metadata.orm as metaorm
 from metadata.admin_cmd import main, essync, escreate
 
 
@@ -21,21 +18,18 @@ class TestAdminTool(TestCase):
         main('essync')
         self.assertTrue(test_patch.called)
 
-    def test_es_commands(self):
+    @patch('metadata.orm.try_db_connect')
+    def test_es_commands(self, test_patch):
         """Test the essync sub command."""
         os.environ['ELASTIC_INDEX'] = 'test_pacifica_index'
+        test_patch.return_value = True
         skip_args = Namespace()
         reg_args = Namespace()
         setattr(skip_args, 'skip_mappings', True)
         setattr(reg_args, 'skip_mappings', False)
         setattr(skip_args, 'threads', 8)
         setattr(reg_args, 'threads', 8)
-        with test_database(SqliteDatabase(':memory:'), metaorm.ORM_OBJECTS):
-            metaorm.DB = SqliteDatabase(':memory:')
-            escreate(skip_args)
-            metaorm.DB = SqliteDatabase(':memory:')
-            escreate(reg_args)
-            essync(reg_args)
-        # pylint: disable=no-member
-        self.assertEqual(skip_args.threads, 8)
-        # pylint: enable=no-member
+        escreate(skip_args)
+        escreate(reg_args)
+        essync(reg_args)
+        self.assertTrue(test_patch.called)
