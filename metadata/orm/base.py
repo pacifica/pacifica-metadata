a8c2be961d5339be81e8d33987603eec20f64a8a
David Brown <dmlb2000@gmail.com>
2018-02-02 14:11:26 -0800

    this adds a recursion limit (#115)
    * this adds a recursion limit
    
    * allow get args to set recursion limit
    
    * try to get rid of one of the issues with code climate
    
    * fix some of the testing
    
    * this sets recursion_limit similar to depth
    
    * just grab the indexed keys
    
    * better cache the value and don't do multiple selects if the count is 0
    
    * I'm not sure that this is what I thought

--- metadata/orm/base.py
+++ metadata/orm/base.py
@@ -123,6 +123,7 @@ class PacificaModel(Model):
     def to_hash(self, **flags):
         """Convert the base object fields into serializable attributes in a hash."""
         recursion_depth = flags.get('recursion_depth', 0)
+        recursion_limit = flags.get('recursion_limit', 1000)
         obj = {}
         obj['created'] = self.created.isoformat()
         obj['updated'] = self.updated.isoformat()
@@ -131,20 +132,22 @@ class PacificaModel(Model):
         obj['_id'] = index_hash(obj['created'], obj['updated'], obj['deleted'])
         if recursion_depth:
             for attr in set(self.cls_revforeignkeys()) - set(flags.get('recursion_exclude', [])):
-                rec_flags = flags.copy()
-                rec_flags['recursion_depth'] -= 1
-                obj.update(self._build_object(attr))
-
+                list_count = getattr(self, attr).count()
+                if list_count > recursion_limit:
+                    obj[attr] = None
+                elif list_count == 0:
+                    obj[attr] = []
+                else:
+                    obj.update(self._build_object(attr))
         return obj
 
     def _build_object(self, attr):
         obj = {attr: []}
         fk_obj_list = {}
-        my_class = self.__class__
         for obj_ref in getattr(self, attr):
             if not fk_obj_list:
                 fk_item_name, fk_obj_list = self._generate_fk_obj_list(obj_ref)
-            obj[attr].append(my_class.get_append_item(
+            obj[attr].append(self.get_append_item(
                 obj_ref, fk_item_name, fk_obj_list))
         return obj
 
@@ -200,24 +203,20 @@ class PacificaModel(Model):
 
     def where_clause(self, kwargs):
         """PeeWee specific extension meant to be passed to a PeeWee get or select."""
-        my_class = self.__class__
         where_clause = Expression(1, OP.EQ, 1)
         if 'deleted' in kwargs:
             if kwargs['deleted'] is None:
                 where_clause &= Expression(
-                    getattr(my_class, 'deleted'), OP.IS, None)
+                    getattr(self.__class__, 'deleted'), OP.IS, None)
             else:
                 date_obj = datetime_converts(kwargs['deleted'])
                 where_clause &= Expression(
-                    getattr(my_class, 'deleted'), OP.EQ, date_obj)
+                    getattr(self.__class__, 'deleted'), OP.EQ, date_obj)
         for date in ['updated', 'created']:
             if date in kwargs:
-                # pylint: disable=protected-access
-                date_obj, date_oper = my_class._date_operator_compare(
-                    date, kwargs)
-                # pylint: enable=protected-access
+                date_obj, date_oper = self._date_operator_compare(date, kwargs)
                 where_clause &= Expression(
-                    getattr(my_class, date), date_oper, date_obj)
+                    getattr(self.__class__, date), date_oper, date_obj)
         return where_clause
 
     @classmethod
