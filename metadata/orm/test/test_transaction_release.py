719d01000433b132439bc7a1cdbbc1f8b5f3e3e2
Ken Auberry <kauberry@mac.com>
2018-06-15 16:09:17 -0700

    Testing fix

--- metadata/orm/test/test_transaction_release.py
+++ metadata/orm/test/test_transaction_release.py
@@ -10,7 +10,6 @@ from metadata.orm.transactions import Transactions
 from metadata.orm.test.test_users import SAMPLE_USER_HASH as SAMPLE_CREATOR_HASH
 
 SAMPLE_TRANS_RELEASE_HASH = {
-    '_id': 1,
     'authorized_person': SAMPLE_CREATOR_HASH['_id'],
     'transaction': SAMPLE_TRANSACTION_HASH['_id']
 }
