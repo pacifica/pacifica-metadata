130add20d4bfc07681f566201135e8a196c18a03
David Brown <dmlb2000@gmail.com>
2018-08-08 20:27:39 -0700

    the connection may work with localhost better

--- travis/unit-tests.sh
+++ travis/unit-tests.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 export NOTIFICATIONS_URL=http://127.0.0.1:8080
-export PEEWEE_URL=postgres://postgres:@127.0.0.1:5432/pacifica_metadata
+export PEEWEE_URL=postgres://postgres:@localhost/pacifica_metadata
 python test_files/cherrypy_catch.py &
 coverage run --include='pacifica/*' -m pytest -vx
 coverage run --include='pacifica/*' -a -m pacifica.metadata --stop-after-a-moment
