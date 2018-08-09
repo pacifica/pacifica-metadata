fff3daee21d1b23667d2c66842b10d450bc56a7f
David Brown <dmlb2000@gmail.com>
2018-08-08 20:19:30 -0700

    fail quickly so I can see what's going on

--- travis/unit-tests.sh
+++ travis/unit-tests.sh
@@ -2,7 +2,7 @@
 export NOTIFICATIONS_URL=http://127.0.0.1:8080
 export PEEWEE_URL=postgres://postgres:@127.0.0.1:5432/pacifica_metadata
 python test_files/cherrypy_catch.py &
-coverage run --include='pacifica/*' -m pytest -v
+coverage run --include='pacifica/*' -m pytest -vx
 coverage run --include='pacifica/*' -a -m pacifica.metadata --stop-after-a-moment
 coverage report --show-missing --fail-under 100
 if [[ $CODECLIMATE_REPO_TOKEN ]] ; then
