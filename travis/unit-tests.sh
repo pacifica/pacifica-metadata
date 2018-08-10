bf74c768ca412b9aa6b5a6e872e5eb57c27ae1a2
David Brown <dmlb2000@gmail.com>
2018-08-09 17:54:15 -0700

    get the right rename paths

--- travis/unit-tests.sh
+++ travis/unit-tests.sh
@@ -2,8 +2,8 @@
 export NOTIFICATIONS_URL=http://127.0.0.1:8080
 export PEEWEE_URL=postgres://postgres:@localhost/pacifica_metadata
 python test_files/cherrypy_catch.py &
-coverage run --include='pacifica/*' -m pytest -vx metadata/orm metadata/elastic metadata/test
-coverage run --include='pacifica/*' -a -m pytest -vx metadata/rest
+coverage run --include='pacifica/*' -m pytest -vx pacifica/metadata/orm pacifica/metadata/elastic pacifica/metadata/test
+coverage run --include='pacifica/*' -a -m pytest -vx pacifica/metadata/rest
 coverage run --include='pacifica/*' -a -m pacifica.metadata --stop-after-a-moment
 coverage report --show-missing --fail-under 100
 if [[ $CODECLIMATE_REPO_TOKEN ]] ; then
