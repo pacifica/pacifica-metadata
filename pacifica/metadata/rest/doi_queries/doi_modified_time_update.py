8f6face17253e4decb66a295efe282e3d90bf2f4
Ken Auberry <kauberry@mac.com>
2018-12-10 14:21:12 -0800

    Extra import statement removed

--- pacifica/metadata/rest/doi_queries/doi_modified_time_update.py
+++ pacifica/metadata/rest/doi_queries/doi_modified_time_update.py
@@ -2,7 +2,7 @@
 # -*- coding: utf-8 -*-
 """CherryPy DOI Registration Updater object class."""
 from __future__ import print_function
-from cherrypy import tools, request, HTTPError
+from cherrypy import tools, request
 from pacifica.metadata.orm.utils import datetime_now_nomicrosecond
 from pacifica.metadata.rest.doi_queries.doi_registration_base import DOIRegistrationBase
 from pacifica.metadata.orm import DOIEntries
