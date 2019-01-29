fbae0fda5ca9def8d8c53a7c2ee36ed723c558be
Ken Auberry <kauberry@mac.com>
2019-01-28 17:32:16 -0800

    Ok. This time for sure!

--- pacifica/metadata/rest/transaction_queries/query_base.py
+++ pacifica/metadata/rest/transaction_queries/query_base.py
@@ -97,8 +97,8 @@ class QueryBase(object):
         results = {}
         for trans in transactions:
             results[trans.id.id] = {
-                'total_file_size_bytes': int(getattr(trans, 'file_size_bytes', 0)),
-                'total_file_count': int(getattr(trans, 'file_count', 0))
+                'total_file_size_bytes': int(trans.file_size_bytes or 0),
+                'total_file_count': int(trans.file_count or 0)
             }
         return results
 
