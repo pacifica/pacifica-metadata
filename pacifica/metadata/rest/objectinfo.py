c72dfe48ae04519a1b5182ebbedeb3bf1f8053de
David Brown <dmlb2000@gmail.com>
2019-04-26 08:40:13 -0700

    Add introspection (#219)
    This changes some objectinfo parameters to add introspection to
    determine the field types.
    
    Signed-off-by: David Brown <dmlb2000@gmail.com>

--- pacifica/metadata/rest/objectinfo.py
+++ pacifica/metadata/rest/objectinfo.py
@@ -11,18 +11,20 @@ from pacifica.metadata.orm.base import db_connection_decorator
 class ObjectInfoAPI(object):
     """ObjectInfoAPI API."""
 
+    lower_obj = {
+        obj.__module__.split('.').pop().lower(): obj.__name__ for obj in orm_obj_module.ORM_OBJECTS
+    }
     exposed = True
 
-    @staticmethod
-    def get_class_object_from_name(object_class_name):
+    @classmethod
+    def get_class_object_from_name(cls, object_class_name):
         """Return a metadata model class for a given class name string."""
         if object_class_name is not None:
-            lower_obj = {
-                obj.__module__.split('.').pop().lower(): obj.__name__ for obj in orm_obj_module.ORM_OBJECTS
-            }
             try:
                 myclass = getattr(
-                    orm_obj_module, lower_obj[object_class_name.lower()])
+                    orm_obj_module,
+                    cls.lower_obj[object_class_name.lower()]
+                )
             except KeyError:
                 myclass = None
         else:
@@ -32,20 +34,17 @@ class ObjectInfoAPI(object):
 
     # CherryPy requires these named methods.
     # pylint: disable=invalid-name, protected-access
-    @staticmethod
+    @classmethod
     @tools.json_out()
     @db_connection_decorator
-    def GET(object_class_name=None, operation=None, **where_clause):
+    def GET(cls, object_class_name=None, operation=None, **where_clause):
         """
         Implement the GET HTTP method.
 
         Returns the json object based on fields passed into kwargs.
         """
         if object_class_name == 'list':
-            lower_obj = {
-                obj.__name__: obj.__module__.split('.').pop().lower() for obj in orm_obj_module.ORM_OBJECTS
-            }
-            return {'available_objects': lower_obj}
+            return {'available_objects': cls.lower_obj}
 
         myclass = ObjectInfoAPI.get_class_object_from_name(object_class_name)
         if operation == 'hashlist':
@@ -56,13 +55,10 @@ class ObjectInfoAPI(object):
             }
         else:  # operation is None or operation == 'overview':
             if myclass is None:
-                js_object = {}
                 if object_class_name is not None:
-                    message = "'{0}' is not a valid class object name".format(
-                        object_class_name)
-                else:
-                    message = 'No object class name found'
-                raise cherrypy.HTTPError(404, message)
+                    message = "'{0}' is not a valid class object name".format(object_class_name)
+                    raise cherrypy.HTTPError(404, message)
+                return list(cls.lower_obj.keys())
             else:
                 js_object = myclass.get_object_info(where_clause)
         return js_object
